1ec59913bddd5806d8f31a0f8b3b9889dbc1abe9
aryanjverma <111136645+aryanjverma@users.noreply.github.com>
2025-01-24 02:16:19 +0000

    tried fixing problems with pocketbase, didn't work

--- public/addTask.js
+++ public/addTask.js
@@ -1,4 +1,4 @@
-import PocketBase from "./pocketbase"
+import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 const taskForm = document.getElementById('taskForm');
@@ -6,10 +6,9 @@ const taskForm = document.getElementById('taskForm');
         
         const taskInput = document.getElementById('taskInput');
         const columns = document.querySelectorAll('.column');
-        const taskDescriptionInput = document.getElementById('taskDescription')
+        const taskDescriptionInput = document.getElementById('taskDescription');
         const fileInput = document.getElementById('taskFiles');
         const taskDeadlineInput = document.getElementById('taskDeadline');
-        
         const record = await pb.collection('bounty').create({
             name: taskInput,
             description: taskDescriptionInput,
@@ -17,6 +16,7 @@ const taskForm = document.getElementById('taskForm');
             additional_documents: fileInput,
             date: taskDeadlineInput
         });
+        console.log(record)
         
 
         taskForm.addEventListener('submit', function(event) {
